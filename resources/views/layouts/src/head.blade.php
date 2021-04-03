ae9fa3a4b654db903f71667ab2dd04b13aee8301
Leiner Ortega <leiner@singleclick.com.co>
2021-04-03 11:57:43 -0500

    install template

---
+++ resources/views/layouts/src/head.blade.php
@@ -0,0 +1,17 @@
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Factory</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon.png') }}">
+    <link href="{{ asset('assets/vendor/jqvmap/css/jqvmap.min.css') }}" rel="stylesheet">
+	<link rel="stylesheet" href="{{ asset('assets/vendor/chartist/css/chartist.min.css') }}">
+    <link href="{{ asset('assets/vendor/bootstrap-select/dist/css/bootstrap-select.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+	<link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
+
+    @yield('myStyles')
+
+</head>
