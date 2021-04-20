cd14e8bdd92b53fa55cf998666dde8bc0d32cbfc
Blist3r <luisdanielr2010@hotmail.com>
2021-04-20 14:21:54 -0500

    Agregada la tabla Users #2

---
+++ public/assets/js/users.js
@@ -0,0 +1,31 @@
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+function EditarUser(id) {
+    $.ajax({
+        url: 'users/show',
+        type: 'POST',
+        data: { id:id },
+        success: function (data) {
+            $("#modalAgregarUser").modal('show');
+            $('#nombre').val(data.nombre);
+            $('#id').val(data.id);
+        }
+    });
+}
+
+function EliminarUser(id) {
+    if (confirm('Seguro desea eliminar el usuario?')) {
+
+        window.location.href='users/delete/' + id;
+        
+    }
+}
+
+function LimpiarInput() {
+    $('#nombre').val('');
+    $('#id').val('');
+}
