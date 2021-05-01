ecfbda2a9fa8592441dc5fa21a4468fd8bb49a66
Blist3r <luisdanielr2010@hotmail.com>
2021-05-01 12:15:26 -0500

    Se agregaron permisos de usuario. Actualmente 'admin ->  universal'

--- routes/web.php
+++ routes/web.php
@@ -30,27 +30,30 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/', function () {
         return view('welcome');
     });
-    // Routas Configuracion
-    // Sedes
-    Route::get('/configuracion/sedes', [App\Http\Controllers\ConfiguracionController::class, 'index'])->name('sedes');
-    Route::post('/configuracion/sedes/create', [App\Http\Controllers\ConfiguracionController::class, 'create'])->name('sedes.create');
-    Route::post('/configuracion/sedes/show', [App\Http\Controllers\ConfiguracionController::class, 'show'])->name('sedes.show');
-    Route::get('/configuracion/sedes/delete/{id}', [App\Http\Controllers\ConfiguracionController::class, 'delete'])->name('sedes.delete');
-    //Usuarios
-    Route::get('/configuracion/users', [App\Http\Controllers\UsersController::class, 'index'])->name('users');
-    Route::post('/configuracion/users/create', [App\Http\Controllers\UsersController::class, 'create'])->name('users.create');
-    Route::post('/configuracion/users/show', [App\Http\Controllers\UsersController::class, 'show'])->name('users.show');
-    Route::get('/configuracion/users/delete/{id}', [App\Http\Controllers\UsersController::class, 'delete'])->name('users.delete');
-    //Categorias
-    Route::get('/configuracion/categorias', [App\Http\Controllers\ConfiguracionController::class, 'index_categorias'])->name('categorias');
-    Route::post('/configuracion/categorias/create', [App\Http\Controllers\ConfiguracionController::class, 'create_categorias'])->name('categorias.create');
-    Route::post('/configuracion/categorias/show', [App\Http\Controllers\ConfiguracionController::class, 'show_categorias'])->name('categorias.show');
-    Route::get('/configuracion/categorias/delete/{id}', [App\Http\Controllers\ConfiguracionController::class, 'delete_categorias'])->name('categorias.delete');
-    //Productos
-    Route::get('/configuracion/productos', [App\Http\Controllers\ProductosController::class, 'index'])->name('productos');
-    Route::post('/configuracion/productos/create', [App\Http\Controllers\ProductosController::class, 'create'])->name('productos.create');
-    Route::post('/configuracion/productos/show', [App\Http\Controllers\ProductosController::class, 'show'])->name('productos.show');
-    Route::get('/configuracion/productos/delete/{id}', [App\Http\Controllers\ProductosController::class, 'delete'])->name('productos.delete');
+    //Valida que el usuario tenga el rol en este caso "admin"
+    Route::group(['middleware' => ['role:admin']], function () {
+        // Routas Configuracion
+        // Sedes
+        Route::get('/configuracion/sedes', [App\Http\Controllers\ConfiguracionController::class, 'index'])->name('sedes');
+        Route::post('/configuracion/sedes/create', [App\Http\Controllers\ConfiguracionController::class, 'create'])->name('sedes.create');
+        Route::post('/configuracion/sedes/show', [App\Http\Controllers\ConfiguracionController::class, 'show'])->name('sedes.show');
+        Route::get('/configuracion/sedes/delete/{id}', [App\Http\Controllers\ConfiguracionController::class, 'delete'])->name('sedes.delete');
+        //Usuarios
+        Route::get('/configuracion/users', [App\Http\Controllers\UsersController::class, 'index'])->name('users');
+        Route::post('/configuracion/users/create', [App\Http\Controllers\UsersController::class, 'create'])->name('users.create');
+        Route::post('/configuracion/users/show', [App\Http\Controllers\UsersController::class, 'show'])->name('users.show');
+        Route::get('/configuracion/users/delete/{id}', [App\Http\Controllers\UsersController::class, 'delete'])->name('users.delete');
+        //Categorias
+        Route::get('/configuracion/categorias', [App\Http\Controllers\ConfiguracionController::class, 'index_categorias'])->name('categorias');
+        Route::post('/configuracion/categorias/create', [App\Http\Controllers\ConfiguracionController::class, 'create_categorias'])->name('categorias.create');
+        Route::post('/configuracion/categorias/show', [App\Http\Controllers\ConfiguracionController::class, 'show_categorias'])->name('categorias.show');
+        Route::get('/configuracion/categorias/delete/{id}', [App\Http\Controllers\ConfiguracionController::class, 'delete_categorias'])->name('categorias.delete');
+        //Productos
+        Route::get('/configuracion/productos', [App\Http\Controllers\ProductosController::class, 'index'])->name('productos');
+        Route::post('/configuracion/productos/create', [App\Http\Controllers\ProductosController::class, 'create'])->name('productos.create');
+        Route::post('/configuracion/productos/show', [App\Http\Controllers\ProductosController::class, 'show'])->name('productos.show');
+        Route::get('/configuracion/productos/delete/{id}', [App\Http\Controllers\ProductosController::class, 'delete'])->name('productos.delete');
+    });
      //Clientes
      Route::get('/configuracion/clientes', [App\Http\Controllers\ClientesController::class, 'index'])->name('clientes');
      Route::post('/configuracion/clientes/create', [App\Http\Controllers\ClientesController::class, 'create'])->name('clientes.create');
